4f2be59cf5dcbd0a50af8d54c3ab799f08e1232f
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-16 17:47:37 +0100

    Signature for Instructions

--- src/main/java/fr/unice/polytech/si3/dda/instruction/DeliverInstruction.java
+++ src/main/java/fr/unice/polytech/si3/dda/instruction/DeliverInstruction.java
@@ -8,5 +8,26 @@ package fr.unice.polytech.si3.dda.instruction;
  * @author Pierre RAINERO
  * @author Joël CANCELA VAZ
  */
-public class DeliverInstruction implements IInstruction{
+public class DeliverInstruction implements IInstruction {
+	int droneNumber;
+	int orderNumber;
+	int productType;
+	int numberOfProducts;
+
+	public DeliverInstruction(int droneNumber, int orderNumber, int productType, int numberOfProducts) {
+		this.droneNumber = droneNumber;
+		this.orderNumber = orderNumber;
+		this.productType = productType;
+		this.numberOfProducts = numberOfProducts;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return droneNumber + " " + "D" + " " + orderNumber + " " + productType + " " + numberOfProducts;
+	}
 }
