2501eb69a945d667deb0d8b9caeb2683043071ff
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-16 17:38:45 +0100

    Fixed main warning, added bat for execution, first classes for instructions

---
+++ src/main/java/fr/unice/polytech/si3/dda/instruction/DeliverInstruction.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.si3.dda.instruction;
+
+/**
+ * Class DeliverInstruction
+ *
+ * @author Jeremy JUNAC
+ * @author Alexandre HILTCHER
+ * @author Pierre RAINERO
+ * @author Joël CANCELA VAZ
+ */
+public class DeliverInstruction implements IInstruction{
+}
---
+++ src/main/java/fr/unice/polytech/si3/dda/instruction/UnloadInstruction.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.si3.dda.instruction;
+
+/**
+ * Class UnloadInstruction
+ *
+ * @author Jeremy JUNAC
+ * @author Alexandre HILTCHER
+ * @author Pierre RAINERO
+ * @author Joël CANCELA VAZ
+ */
+public class UnloadInstruction implements IInstruction {
+}
