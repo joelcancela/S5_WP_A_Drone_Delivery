5e7656c9f5e90a55ef413c60524d220fa1e50cf2
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-16 17:52:10 +0100

    Added instructions

--- src/main/java/fr/unice/polytech/si3/dda/instruction/UnloadInstruction.java
+++ src/main/java/fr/unice/polytech/si3/dda/instruction/UnloadInstruction.java
@@ -9,6 +9,25 @@ package fr.unice.polytech.si3.dda.instruction;
  * @author Joël CANCELA VAZ
  */
 public class UnloadInstruction implements IInstruction {
+	int droneNumber;
+	int idWarehouse;
+	int productType;
+	int numberOfProducts;
+
 	public UnloadInstruction(int droneNumber, int idWarehouse, int productType, int numberOfProducts) {
+		this.droneNumber = droneNumber;
+		this.idWarehouse = idWarehouse;
+		this.productType = productType;
+		this.numberOfProducts = numberOfProducts;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return droneNumber + " " + "U" + " " + idWarehouse + " " + productType + " " + numberOfProducts;
 	}
 }
