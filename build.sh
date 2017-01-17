74c7117b8343621bca3c6a281ecc1a98833df6f5
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-17 19:51:05 +0100

    final commit milestone

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mvn clean install
-mvn -q exec:java -pl scheduler -Dexec.args="$*"
+mvn -e exec:java -pl scheduler -Dexec.args="$*"
 mvn -q exec:java -pl visualiser -Dexec.args="$* scheduler.out"
 #mvn -q exec:java -pl benchmark -Dexec.args="$* scheduler.out"
