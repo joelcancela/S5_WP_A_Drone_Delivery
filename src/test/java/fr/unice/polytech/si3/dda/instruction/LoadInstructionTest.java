cd6a3ae4456780f5523b18ca973c83a4a16654b9
Jeremy Junac <bombers_06@hotmail.fr>
2017-01-17 13:48:04 +0100

    Some test and exception if product is out of stock

--- src/test/java/fr/unice/polytech/si3/dda/instruction/LoadInstructionTest.java
+++ src/test/java/fr/unice/polytech/si3/dda/instruction/LoadInstructionTest.java
@@ -13,20 +13,20 @@ import org.junit.rules.TemporaryFolder;
 
 import fr.unice.polytech.si3.dda.ContextParser;
 import fr.unice.polytech.si3.dda.exception.ProductNotFoundException;
-import fr.unice.polytech.si3.dda.instruction.DeliverInstruction;
+import fr.unice.polytech.si3.dda.mapping.Warehouse;
 import fr.unice.polytech.si3.dda.order.Product;
 import fr.unice.polytech.si3.dda.scheduler.Context;
 import fr.unice.polytech.si3.dda.scheduler.Drone;
 import fr.unice.polytech.si3.dda.util.Coordinates;
 
-public class DeliverInstructionTest {
-
+public class LoadInstructionTest {
+	
     @Rule
     public TemporaryFolder temp = new TemporaryFolder();
 
     String FILE_NAME = "new-file.txt";
     File file;
-    DeliverInstruction inst;
+    LoadInstruction inst;
     Context ctx;
 	
 	@Before
@@ -49,24 +49,24 @@ public class DeliverInstructionTest {
 	}
 	
 	@Test
-	public void testExecute() throws Exception{
+	public void testExecute() throws Exception {
 		Drone d = ctx.getFleet().getDrone(0);
-		d.load(new Product(100, 0));
+		Warehouse w = ctx.getMap().getWarehouse(0);
 		d.move(new Coordinates(1,0));
-		inst = new DeliverInstruction(0, 0, 0, 1);
+		inst = new LoadInstruction(0, 0, 0, 1);
 		assertEquals(2, inst.execute(ctx));
-		assertEquals(new Coordinates(1,1), d.getCoordinates());
+		assertEquals(new Coordinates(0,0), d.getCoordinates());
 	}
 	
 	@Test(expected=ProductNotFoundException.class)
-	public void testExecuteFail() throws Exception{
+	public void testExecuteFail() throws Exception {
 		Drone d = ctx.getFleet().getDrone(0);
+		Warehouse w = ctx.getMap().getWarehouse(0);
 		d.move(new Coordinates(1,0));
-		inst = new DeliverInstruction(0, 0, 0, 1);
+		inst = new LoadInstruction(0, 0, 2, 1);
 		assertEquals(2, inst.execute(ctx));
-		assertEquals(new Coordinates(1,1), d.getCoordinates());
+		assertEquals(new Coordinates(0,0), d.getCoordinates());
 	}
-
 
 	@Test
 	public void testToString() {
---
+++ src/test/java/fr/unice/polytech/si3/dda/instruction/WaitInstructionTest.java
@@ -0,0 +1,24 @@
+package fr.unice.polytech.si3.dda.instruction;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaitInstructionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
