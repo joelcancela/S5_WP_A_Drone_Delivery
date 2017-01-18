cb4e74c9d3bd4bea674512b892183971bc4d44af
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-18 21:00:01 +0100

    Login done

---
+++ AmIDone_visualiser/js/index.js
@@ -0,0 +1,45 @@
+var customerView = "customerView/customerView.html";
+var operatorView = "operatorView/operatorView.html";
+
+var init = function () {
+    twoWayLoginEvents();
+};
+
+var twoWayLoginEvents = function () {
+    var customerRadioBtn = document.getElementById("custRadio");
+    var customerSubBtn = document.getElementById("custSub");
+    var operatorRadioBtn = document.getElementById("opRadio");
+    var operatorSubBtn = document.getElementById("opSub");
+    var customerForm = document.getElementById("custForm");
+    var operatorForm = document.getElementById("opForm");
+
+    customerRadioBtn.onchange = function () {
+        operatorRadioBtn.checked = !this.checked;
+        operatorSubBtn.disabled = this.checked;
+        customerSubBtn.disabled = !this.checked;
+
+    };
+    operatorRadioBtn.onchange = function () {
+        customerRadioBtn.checked = !this.checked;
+        customerSubBtn.disabled = this.checked;
+        operatorSubBtn.disabled = !this.checked;
+
+    };
+
+    customerForm.onsubmit = function () {
+        var customerId = $('#customerId').val();
+        window.location = window.location.href + (customerView + "?customerId=" + customerId);
+        return false;
+    };
+
+    operatorForm.onsubmit = function () {
+        window.location = window.location.href + (operatorView);
+        return false;
+    };
+};
+
+
+//Initialisation call
+$('document').ready(function () {
+    init();
+});
