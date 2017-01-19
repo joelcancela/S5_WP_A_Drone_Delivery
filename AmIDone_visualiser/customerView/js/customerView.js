8d47a8e5f8251b58b7b6c3cea59383bba8648c4b
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-19 09:09:14 +0100

    fixed benchmark

--- AmIDone_visualiser/customerView/js/customerView.js
+++ AmIDone_visualiser/customerView/js/customerView.js
@@ -1,10 +1,24 @@
+var customerId;
+
 var init = function () {
     setVars();
 };
 
 var setVars = function () {
-
+    customerId = console.log(getQueryVariable("customerId"));
 };
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
 
 
 //Initialisation call
