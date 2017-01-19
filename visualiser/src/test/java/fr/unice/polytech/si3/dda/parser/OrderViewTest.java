d2bb871bcb13d7535dfb6c6f4c49011a6f71a4ad
Alexandre <alex.hiltcher@gmail.com>
2017-01-19 12:46:25 +0100

    parser fro Json

---
+++ visualiser/src/test/java/fr/unice/polytech/si3/dda/parser/OrderViewTest.java
@@ -0,0 +1,20 @@
+package fr.unice.polytech.si3.dda.parser;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alexh on 18/01/2017.
+ */
+public class OrderViewTest {
+    @Test
+    public void toJson() throws Exception {
+        OrderView orderView = new OrderView("../examples/contextFoncManyOrders.in", "../scheduler.out");
+        System.out.println(orderView.toJson());
+
+    }
+
+}
