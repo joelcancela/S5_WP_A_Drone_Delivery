3460f034e7258bd3594fccb06507163ef2028be6
Joël CANCELA VAZ <joel.cancela-vaz@etu.unice.fr>
2017-01-20 17:39:19 +0100

    Scripts, readme

--- chainExec.sh
+++ chainExec.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Keep the cd as root project
-mvn clean install
+#Don't forget to mvn clean install
 mvn -q exec:java -pl scheduler -Dexec.args="$*"
+echo ""
 mvn -q exec:java -pl visualiser -Dexec.args="$* scheduler.out"
+echo ""
 mvn -q exec:java -pl benchmark -Dexec.args="$* scheduler.out"
